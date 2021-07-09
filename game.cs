07a68daba9c05bb1d24905d2e383af8d68d1c81f
jeremiah booth <jeremiah.boothphotog@gmail.com>
2021-07-09 08:58:57 -0700

    init

---
+++ game.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RPSLS
+{
+    public class Game
+    {
+        //member variables
+        public int rounds = 0;
+        public          
+    }
+}
---
+++ program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace RPSLS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //add the call to the game here.
+
+        }
+    }
+}
